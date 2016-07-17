7f3288efb717cafa2017c188d8522d1ff594031c
jeus <alkhandani@gmail.com>
2016-07-17 19:06:25 +0430

    Add unit test for benchmark deserializer

--- src/test/java/com/jeus/serde/main/BenchTest.java
+++ src/test/java/com/jeus/serde/main/BenchTest.java
@@ -6,9 +6,11 @@ package com.jeus.serde.main;
  * and open the template in the editor.
  */
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 import com.jeus.serde.entity.User;
 import com.jeus.serde.main.Serializer1;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
@@ -58,8 +60,8 @@ public class BenchTest {
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
-    @Test
-    public void benchMark() {
+//    @Test
+    public void benchMarkSer() {
         System.out.println("START TEST");
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < NUM_RECORDS; i++) {
@@ -86,7 +88,30 @@ public class BenchTest {
 
         }
         long endTime = System.currentTimeMillis();
-        System.out.println("Producer Performance [Num/sec write]: "+NUM_RECORDS +"/"+ megaBytePerSec(endTime - startTime));
+        System.out.println("Producer Performance [Num/sec write]: " + NUM_RECORDS + "/" + megaBytePerSec(endTime - startTime));
         assertTrue(true);
     }
+
+    @Test
+    public void benchMarkDeser() {
+        System.out.println("START TEST");
+        long startTime = System.currentTimeMillis();
+        User user = new User();
+        for (int i = 0; i < NUM_RECORDS; i++) {
+            try {
+                Kryo kryo = new Kryo();
+                Input input = new Input(new FileInputStream("/home/jeus/project/Datis/kryo_serde/test1/user" + i + ".bin"));
+                user = kryo.readObject(input, User.class);
+                input.close();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Serializer1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+        System.out.println(user.toString());
+        long endTime = System.currentTimeMillis();
+        System.out.println("Producer Performance [Num/sec write]: " + NUM_RECORDS + "/" + megaBytePerSec(endTime - startTime));
+        assertTrue(true);
+    }
+
 }
